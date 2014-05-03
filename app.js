ce5cf84687dacac237798907356f56d504421342
Michael Kelly <mkelly@mozilla.com>
2014-05-03 11:16:45 -0400

    Websockets AND HTTP. Living together as one!

--- app.js
+++ app.js
@@ -1,20 +1,7 @@
 /*jslint node: true */
 'use strict';
+var core = require('./lib/core');
 
-var express = require('express');
-var nunjucks = require('nunjucks');
 
-var app = express();
-
-nunjucks.configure('views', {
-    autoescape: true,
-    express: app
-});
-
-app.get('/', function(req, res){
-  res.render('index.html');
-});
-
-var server = app.listen(3000, function() {
-    console.log('Listening on port %d', server.address().port);
-});
+var frostPatrol = new core.FrostPatrol();
+frostPatrol.start();
